907023a46176a1fd07fccac8f84deefda350fa46
esurur <emanbsurur14@gmail.com>
2023-02-06 15:26:10 -0500

    commited

---
+++ HelloWorld/src/com/craft/GitDemo.java
@@ -0,0 +1,13 @@
+package com.craft;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Helloo world");
+		System.out.println("Hel1lo wwoorrlldd");
+
+	}
+
+}
